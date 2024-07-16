2f6d0af2067586bc78c7278e56dfe5574ea45cdf
Mehul721 <jmehul721@gmail.com>
2024-07-16 15:32:28 +0530

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Mehul Jain on 16/07/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
