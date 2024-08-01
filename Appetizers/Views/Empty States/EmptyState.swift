199e141a650bc23a55b64a2dc14145da23da68f0
Mehul721 <jmehul721@gmail.com>
2024-08-01 23:50:29 +0530

    Added EmptyState for an empty OrderView

---
+++ Appetizers/Views/Empty States/EmptyState.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyState.swift
+//  Appetizers
+//
+//  Created by Mehul Jain on 01/08/24.
+//
+
+import SwiftUI
+
+struct EmptyState: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmptyState()
+}
