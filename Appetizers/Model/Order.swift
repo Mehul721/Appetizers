52363dc454d853c244e80036c68e86b882efe3c0
Mehul721 <jmehul721@gmail.com>
2024-08-03 23:49:04 +0530

    @EnvironmentObject in OrderView completed.

---
+++ Appetizers/Model/Order.swift
@@ -0,0 +1,18 @@
+//
+//  Order.swift
+//  Appetizers
+//
+//  Created by Mehul Jain on 03/08/24.
+//
+
+import SwiftUI
+
+struct Order: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Order()
+}
